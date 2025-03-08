981b65e24f0eecc9a3c6fb5107bd4c3b583979cd
Luis-Jones-Coder <n16e3tnn@students.codeinstitute.net>
2025-03-08 17:45:52 +0000

    Create each method to listen for click events, iterate through all cards, and add a function to rotate the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-alert(3334)
+$(document).ready(function (){
+    const cards = $(".card");
+    // Creating a function that
+    const flipIt = (e) => {
+        let card = $(e.currentTarget); // Transform e  to a objet  
+        card.addClass("flip");
+        // console.log(e.target);
+    }
+
+    //  Making all cards listen to the event click to call variable with a function flip
+    cards.each(function() {
+        $(this).on('click', flipIt);
+    });
+});
