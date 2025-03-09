ab5d86accdf3d57515eff8a8ba0eb66fb2575113
Luis-Jones-Coder <n16e3tnn@students.codeinstitute.net>
2025-03-09 11:29:43 +0000

    Fix bug where non-matching cards remain flipped

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     const cards = document.querySelectorAll(".card");
     let card_1, card_2;
+    let disableCards = false;
 
+    // Compare if the images are the same and if they are, clear card_1 and card_2 so that the images remain flipped and stop rotating
     const comparing = (imagen1, imagen2) => {
-        if (imagen1 == imagen2) {
-            return console.log("son iguales");
+        if (imagen1 === imagen2) {
+            // Remove the event listener so that it cannot be clicked
+            card_1.removeEventListener("click", flipIt);
+            card_2.removeEventListener("click", flipIt);
+
+            card_1 = card_2 = "";
+
+            return disableCards = false;
         }
-        
-        // console.log("adding move");
-        // Add the "move" class to card_1 and card_2
-        setTimeout(() =>{
+
+        // Add the "move" class to card_1 and card_2 after 500ms
+        setTimeout(() => {
             card_1.classList.add("move");
             card_2.classList.add("move");
-        })
-       
+        }, 500);
 
         // Remove the "move" class and add the "flip" class after 1500ms
         setTimeout(() => {
-            card_1.classList.remove("move");
-            card_2.classList.remove("move");
-            card_1.classList.add("flip");
-            card_2.classList.add("flip");
+            card_1.classList.remove("move", "flip");
+            card_2.classList.remove("move", "flip");
+
+            card_1 = card_2 = "";
+            disableCards = false;
         }, 1500);
     };
 
@@ -28,20 +35,20 @@ document.addEventListener("DOMContentLoaded", function() {
         let card = e.target;
         card.classList.add("flip");
 
-        
-
         // Ensure that the next click is on a different card than card_1
-        if (card !== card_1) {
+        if (card !== card_1 && !disableCards) {
+            card.classList.add("flip");
             if (!card_1) {
-                return (card_1 = card);
+                return card_1 = card;
             }
             card_2 = card;
+            disableCards = true;
 
             // Getting images
             let img1 = card_1.querySelector('img').src;
             let img2 = card_2.querySelector('img').src;
 
-            comparing(img1,img2);
+            comparing(img1, img2);
         }
     };
 
