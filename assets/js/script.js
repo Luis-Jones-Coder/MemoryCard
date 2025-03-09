8139486eeea8ce2bab3a9c5d7f1017bfbf9da91e
Luis-Jones-Coder <n16e3tnn@students.codeinstitute.net>
2025-03-09 06:39:28 +0000

    Create setTimeout function to allow necessary time for animation to appear and change javascript jquery to pure javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,52 @@
-$(document).ready(function (){
-    const cards = $(".card");
-    // Creating a function that
-    const flipIt = (e) => {
-        let card = $(e.currentTarget); // Transform e  to a objet  
-        card.addClass("flip");
-        // console.log(e.target);
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll(".card");
+    let card_1, card_2;
 
-    //  Making all cards listen to the event click to call variable with a function flip
-    cards.each(function() {
-        $(this).on('click', flipIt);
+    const comparing = (imagen1, imagen2) => {
+        if (imagen1 == imagen2) {
+            return console.log("son iguales");
+        }
+        
+        // console.log("adding move");
+        // Add the "move" class to card_1 and card_2
+        setTimeout(() =>{
+            card_1.classList.add("move");
+            card_2.classList.add("move");
+        })
+       
+
+        // Remove the "move" class and add the "flip" class after 1500ms
+        setTimeout(() => {
+            card_1.classList.remove("move");
+            card_2.classList.remove("move");
+            card_1.classList.add("flip");
+            card_2.classList.add("flip");
+        }, 1500);
+    };
+
+    const flipIt = (e) => {
+        let card = e.target;
+        card.classList.add("flip");
+
+        
+
+        // Ensure that the next click is on a different card than card_1
+        if (card !== card_1) {
+            if (!card_1) {
+                return (card_1 = card);
+            }
+            card_2 = card;
+
+            // Getting images
+            let img1 = card_1.querySelector('img').src;
+            let img2 = card_2.querySelector('img').src;
+
+            comparing(img1,img2);
+        }
+    };
+
+    // Make all cards listen to the click event and call the flipIt function
+    cards.forEach(function(card) {
+        card.addEventListener('click', flipIt);
     });
 });
