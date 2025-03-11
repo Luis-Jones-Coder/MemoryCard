f307cb104635b1c496f0860ff2d7e30ddd0672f3
Luis-Jones-Coder <n16e3tnn@students.codeinstitute.net>
2025-03-11 03:29:14 +0000

    Implement versatility in resetGame function to shuffle cards to different positions each time the game restarts

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,22 @@ document.addEventListener("DOMContentLoaded", function() {
     let card_1, card_2;
     let disableCards = false;
     let matching = 0;
-   
+
+    
 
 
     // Compare if the images are the same and if they are, clear card_1 and card_2 so that the images remain flipped and stop rotating
     const comparing = (imagen1, imagen2) => {
         if (imagen1 === imagen2) {
-            // Each time we match a card, 1 will be added to the variable.
+            // Each time we match a card, +1 will be added to the variable.
             matching++
             num_matching.innerHTML = matching;
             if (matching == 8){
-                 return resetGame();
+                // The function of the timeout is to give a second before resetting the cards
+                setTimeout(() => {
+                    return resetGame();
+                }, 1000);
+                
             }
             // Remove the event listener so that it cannot be clicked
             card_1.removeEventListener("click", flipIt);
@@ -52,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             card_2 = card;
             disableCards = true;
-
+ 
             // Getting images
             let img1 = card_1.querySelector('img').src;
             let img2 = card_2.querySelector('img').src;
@@ -65,17 +70,31 @@ document.addEventListener("DOMContentLoaded", function() {
     const resetGame = () =>{
         matching = 0;
         card_1 = card_2 = "";
+        let disableCards = false;
         num_matching.innerHTML = matching;
 
-        cards.forEach(function(card) {
+        // Creating the array to obtain random numbers with the Math.random function
+        let numbers = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+        numbers.sort(() =>{ return Math.random() - 0.5});
+        
+
+        // This function makes it so that each time we restart the game, the cards appear in different positions. 
+        cards.forEach((card,index) => {
             // Make the card flip back to initial position
+           
             card.classList.remove("flip");
+            // Obtaining the images of the cards
+            let img = card.querySelector("img");
+            // Using the image attribute to add the numbers array with an index that would already provide the random number, so that our cards appear in different positions.
+            img.src = `assets/image/${numbers[index]}.png`
             card.addEventListener('click', flipIt);
         });
     }
+    resetGame();
 
     // Make all cards listen to the click event and call the flipIt function
     cards.forEach(function(card) {
+        
         card.addEventListener('click', flipIt);
     });
 });
